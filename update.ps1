80a1a462a1489c13552fa69b76057b23ea997c5a
William Gathoye <william@gathoye.be>
2018-07-12 02:43:18 -0700

    Correct parsing and regex to use built-in package

--- update.ps1
+++ update.ps1
@@ -5,35 +5,36 @@ $releases = 'https://github.com/Securepoint/openvpn-client/releases'
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -Algorithm 'SHA512'}
 
 function global:au_SearchReplace {
-    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]package\s*=\s*)('.*')" = "`$1'$($Latest.url32)'"
-            "(?i)(^\s*package\s*=\s*`"[$]toolsDir\\).*`${1}$($Latest.filename32)`""
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.checksum32)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]package\s*=\s*)""([$]toolsDir\\)(.*"")" = "`$1""`$2$($Latest.FileName32)"""
+      "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.checksum32)'"
     }
+  }
 }
 
 function global:au_GetLatest {
 
-    $downloadedPage = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $downloadedPage = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $baseUrl = $([System.Uri]$releases).Authority
+  $url32 = $downloadedPage.links | ? href -match '.exe$' | select -First 1 -expand href
+  # By default packages hosted on github do not have a full path, but only the
+  # path from the root. Cast the URL to a full URL.
+  $baseUrl = $([System.Uri]$releases).Authority
+  $scheme = $([System.Uri]$releases).Scheme
+  if ($url32.Authority -cnotmatch $baseUrl) {
+    $url32 = $scheme + "://" + $baseUrl + $url32
+  }
+  $url32SegmentSize = $([System.Uri]$url32).Segments.Length
+  $filename32 = $([System.Uri]$url32).Segments[$url32SegmentSize - 1]
 
-    $url32 = $downloadedPage.links | ? href -match '.exe$' | select -First 1 -expand href
-    if ($url32.Authority -cnotmatch $baseUrl) {
-        $url32 = 'https://' + $baseUrl + $url32
-    }
-    $url32SegmentSize = $([System.Uri]$url32).Segments.Length
-    $filename32 = $([System.Uri]$url32).Segments[$url32SegmentSize - 1]
+  $version = [regex]::match($url32,'/[A-Za-z-]+-([0-9]+.[0-9]+.[0-9]+).*exe').Groups[1].Value
 
-    $version = [regex]::match($url32,'/[A-Za-z-]+-([0-9]+.[0-9]+.[0-9]+).*exe').Groups[1].Value
-
-    return @{
-        url32 = $url32;
-        filename32 = $filename32;
-        version = $version;
-    }
+  return @{
+    url32 = $url32;
+    filename32 = $filename32;
+    version = $version;
+  }
 }
 
 update -ChecksumFor none
