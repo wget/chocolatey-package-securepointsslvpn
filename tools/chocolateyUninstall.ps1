08c0db70d7dff17810c3e54d725a00eb185790a0
William Gathoye <william@gathoye.be>
2019-03-23 15:29:22 +0100

    Simplify uninstall process and avoid 0 to be printed on stdout

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,40 +1,20 @@
 $packageName = 'securepointsslvpn'
 $softwareName = 'Securepoint*SSL*VPN*'
 $installerType = 'msi'  
-
 $silentArgs = '/qn /norestart'
 $validExitCodes = @(0, 3010, 1605, 1614, 1641)
 
-$uninstalled = $false
-# Get-UninstallRegistryKey is new to 0.9.10, if supporting 0.9.9.x and below,
-# take a dependency on "chocolatey-uninstall.extension" in your nuspec file.
-# This is only a fuzzy search if $softwareName includes '*'. Otherwise it is 
-# exact. In the case of versions in key names, we recommend removing the version
-# and using '*'.
 [array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
-
 if ($key.Count -eq 1) {
-
-  $file = "$($key.UninstallString)"
-
-  if ($installerType -eq 'MSI') {
-    # The Product Code GUID is all that should be passed for MSI, and very
-    # FIRST, because it comes directly after /x, which is already set in the
-    # Uninstall-ChocolateyPackage msiargs (facepalm).
-    $silentArgs = "$($key.PSChildName) $silentArgs"
-
-    # Don't pass anything for file, it is ignored for msi (facepalm number 2)
-    # Alternatively if you need to pass a path to an msi, determine that and
-    # use it instead of the above in silentArgs, still very first.
-    $file = ''
-  }
+  $silentArgs = "$($key.PSChildName) $silentArgs"
+  $file = ''
 
   Uninstall-ChocolateyPackage `
     -PackageName $packageName `
     -FileType $installerType `
     -SilentArgs "$silentArgs" `
     -ValidExitCodes $validExitCodes `
-    -File "$file"
+    -File "$file" | Out-Null
 
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
